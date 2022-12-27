485b22582e009c18c38c73ed8e61e34c1eb47c79
nimishapatel1987 <nimisha.patel1987@gmail.com>
2022-12-27 21:56:44 +0000

    Week17-maven-restful-booker-restassured-project3_Nimishapatel

---
+++ src/main/java/com/restful/booker/model/LoginPojo.java
@@ -0,0 +1,25 @@
+package com.restful.booker.model;
+
+import java.util.HashMap;
+
+public class LoginPojo {
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+
+    }
+}
